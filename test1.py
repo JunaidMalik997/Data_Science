affbe0c720be8aa90a6d7de105925c87d1b26275
JunaidMalik997 <junaidravian997@gmail.com>
2022-03-25 01:20:47 +0500

    Added Binary Search Function

--- test1.py
+++ test1.py
@@ -5,5 +5,24 @@ def multiplyNumbers(a, b):
         
     return result
 
+def binarysearch(a, b):
+    start = 0
+    end = len(a)
+    mid = (start + end) // 2
+    
+    while start < end:
+        if b < a[mid]:
+            end = mid - 1
+            mid = (start + end) // 2
+        elif b > a[mid]:
+            start = mid + 1
+            mid = (start + end) // 2
+        elif b == a[mid]:
+            return True
+    return False
+
 results = multiplyNumbers(3,4)
 print(results)
+
+boolresult = binarysearch([2,4,6,8,1], 7)
+print(boolresult)
